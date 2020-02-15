cb76000729966af540808e20281538d1055f1d1f
colus3 <colus4@gmail.com>
2020-02-15 23:05:40 +0900

    타이틀 애니메이션 효과 추가

--- components/Text.js
+++ components/Text.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Text = ({ children, icon, iconLeft, iconRight }) => (
-  <div className={icon ? `icon-${iconLeft ? 'left' : 'right'}` : ''}>
+const Text = ({ children, icon, iconLeft, iconRight, onClick, style }) => (
+  <div className={icon ? `icon-${iconLeft ? 'left' : 'right'}` : ''} onClick={onClick} style={style}>
     {iconLeft ? <FontAwesomeIcon icon={icon} /> : ''}
     {children}
     {iconRight ? <FontAwesomeIcon icon={icon} /> : ''}
@@ -14,6 +14,8 @@ Text.propTypes = {
   icon: PropTypes.object,
   iconLeft: PropTypes.bool,
   iconRight: PropTypes.bool,
+  onClick: PropTypes.func,
+  style: PropTypes.object,
 };
 
 export default Text;
--- components/items/TitleContent.js
+++ components/items/TitleContent.js
@@ -1,41 +1,60 @@
 /**
  * Created by colus on 2017. 4. 29..
  */
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Col, Container, Jumbotron, Row } from 'reactstrap';
-import Name from "../Name";
-import Title from "../Title";
-import UserInfo from "./UserInfo";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import Text from "../Text";
+import { faTerminal } from '@fortawesome/free-solid-svg-icons';
+import { Random } from 'react-animated-text';
+import Name from '../Name';
+import Title from '../Title';
+import UserInfo from './UserInfo';
+import Text from '../Text';
 
-const TitleContent = ({ user, shortIntro, onDownload }) => (
-  <Jumbotron fluid>
-    <Container fluid>
-      <Row>
-        <Col xs="12" sm="8" md="5" style={{ marginBottom: '25px' }}>
-          <Name>{user.username}</Name>
-          <Title>{shortIntro}</Title>
-          <Button className="d-print-none" outline color="light" onClick={onDownload}>Download PDF</Button>
-        </Col>
-        <Col className="d-none d-md-block" md="2">
-          <p>
-            <img src="/my-image.png" className="rounded-circle mx-auto d-block my-image" alt="Responsive image"/>
-          </p>
-        </Col>
-        <Col xs="12" sm="4" md="5">
-          <UserInfo user={user}/>
-        </Col>
-      </Row>
-    </Container>
-    <h6 className="d-none d-lg-inline-block">
-      <Text icon={faChevronRight} iconLeft>
-        This page is designed using Node.js, React.js, Redux, Next.js, MariaDB, Bootstrap, Puppeteer, Docker, AWS.
-      </Text>
-    </h6>
-  </Jumbotron>
-);
+const TitleContent = ({ user, shortIntro, onDownload }) => {
+  const [pause, setPause] = useState(true);
+  const onTogglePause = useCallback(() => setPause(!pause), [pause]);
+  return (
+    <Jumbotron fluid>
+      <Container fluid>
+        <Row>
+          <Col xs="12" sm="8" md="5" style={{ marginBottom: '25px' }}>
+            <Name>{user.username}</Name>
+            <Title>{shortIntro}</Title>
+            <Button className="d-print-none" outline color="light" onClick={onDownload}>Download PDF</Button>
+          </Col>
+          <Col className="d-none d-md-block" md="2">
+            <p>
+              <img src="/my-image.png" className="rounded-circle mx-auto d-block my-image" alt="Responsive image"/>
+            </p>
+          </Col>
+          <Col xs="12" sm="4" md="5">
+            <UserInfo user={user}/>
+          </Col>
+        </Row>
+      </Container>
+      <h6 className="d-none d-print-none d-lg-inline-block">
+        <Text icon={faTerminal} iconLeft onClick={onTogglePause} style={{ cursor: 'pointer' }}>
+          {/*{text ? 'This page is designed using Node.js, React.js, Redux, Next.js, MariaDB, Bootstrap, Puppeteer, Docker, AWS.' : ''}*/}
+          <Random
+            text={'This page is designed using Node.js, React.js, Redux, Next.js, MariaDB, Bootstrap, Puppeteer, Docker, AWS.'}
+            paused={pause}
+            iterations={1}
+            effect="verticalFadeIn"
+            effectChange={2}
+            effectDirection="up"
+            style={{float: 'right'}}
+          />
+        </Text>
+      </h6>
+      <h6 className="d-none d-print-block">
+        <Text icon={faTerminal} iconLeft>
+          This page is designed using Node.js, React.js, Redux, Next.js, MariaDB, Bootstrap, Puppeteer, Docker, AWS.
+        </Text>
+      </h6>
+    </Jumbotron>
+  );
+};
 
 TitleContent.propTypes = {
   user: PropTypes.object,
