3da6b5e1439a6973e174c75e2801afa8db5413cb
colus3 <colus4@gmail.com>
2020-02-22 14:12:30 +0900

    환경 변수 수정.3

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = withCSS({
   env: {
     API_URL: 'http://api.myresume.kr:8080',
-    ADMIN_URL: 'http://admin.myresume.kr',
-    RESUME_URL: 'http://myresume.kr',
+    ADMIN_URL: 'http://admin.myresume.kr:3000',
+    RESUME_URL: 'http://myresume.kr:3000',
   }
 });
